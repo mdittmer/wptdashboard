8d7da27bbeb0d9ad2932518fb914caaedf6d38ac
Mark Dittmer <markdittmer@google.com>
2017-11-10 08:30:40 -0500

    Update util script locations

--- util/docker-dev/dev_auth.sh
+++ util/docker-dev/dev_auth.sh
@@ -5,4 +5,4 @@ source "${DOCKER_DIR}/../logging.sh"
 WPTDASHBOARD_DIR=${WPTDASHBOARD_DIR:-"${DOCKER_DIR}/../.."}
 
 info "Ensuring application-default credentials"
-"${DOCKER_DIR}/su_i_exec.sh" /wptdashboard/util/docker/inner/dev_auth.sh
+docker exec -it -u 0:0 wptd-dev-instance /wptdashboard/util/docker-dev/inner/dev_auth.sh
